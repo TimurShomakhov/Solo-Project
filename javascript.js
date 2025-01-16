352653d7c88eba21f060f1fe77f718eb7fa6d274
Timur Shomakhov <timur-colmberg@web.de>
2025-01-16 20:41:44 +0100

    Made adjustments for animations

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 function playGame(playerChoice) {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = ["rock", "paper", "scissors"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-  let result = '';
+  let result = "";
 
   if (playerChoice === computerChoice) {
-      result = `It's a tie! You both chose ${playerChoice}.`;
+    result = `It's a tie! You both chose ${playerChoice}.`;
   } else if (
-      (playerChoice === 'rock' && computerChoice === 'scissors') ||
-      (playerChoice === 'paper' && computerChoice === 'rock') ||
-      (playerChoice === 'scissors' && computerChoice === 'paper')
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-      result = `You win! ${playerChoice} beats ${computerChoice}.`;
+    result = `You win! ${playerChoice} beats ${computerChoice}.`;
   } else {
-      result = `You lose! ${computerChoice} beats ${playerChoice}.`;
+    result = `You lose! ${computerChoice} beats ${playerChoice}.`;
   }
 
-  document.getElementById('result').textContent = result;
+  document.getElementById("result").textContent = result;
 }
